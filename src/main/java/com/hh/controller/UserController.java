dee65073ca05222956a69742923625a476d2d7f5
HieuHoang08 <hth08122k3@gmail.com>
2025-06-15 01:21:42 +0700

    second commit

--- src/main/java/com/hh/controller/UserController.java
+++ src/main/java/com/hh/controller/UserController.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.hh.domain.User;
-import com.hh.repository.UserRepository;
 import com.hh.service.UserService;
 
 @Controller
@@ -17,9 +17,10 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService ) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @RequestMapping("/")
     public String getHomePage(Model model) {
         List<User> arrUsers = this.userService.getAllUserByEmail("1@gmail.com");
@@ -27,18 +28,25 @@ public class UserController {
         model.addAttribute("message", "message");
         return "hello";
     }
+
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        System.out.println("List of users: " + users);
+        model.addAttribute("users1", users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
-    @RequestMapping("/admin/user/create1")
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User newUser) {
         System.out.println("Creating user: " + newUser);
         this.userService.handleSaveUser(newUser);
-        return "hello";
+        return "redirect:/admin/user";
     }
 }
-
-
-
